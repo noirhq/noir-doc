5816ab0c4499899ad81ad7b3e605f567efb353d1
code0xff <ian.jungyong.um@gmail.com>
2023-10-06 17:53:49 +0900

    feat: Add blog theme

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -1,16 +1,54 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from '@jcamp/vitepress-blog-theme/config'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Noir Docs",
   description: "Noir Documents",
   themeConfig: {
+    blog: {
+      title: 'My AI Written Blog',
+      description: 'All these articles were written by AI!',
+      defaultAuthor: 'AI Writer',
+      categoryIcons: {
+        article: 'i-[heroicons-outline/book-open]',
+        tutorial: 'i-[heroicons-outline/academic-cap]',
+        document: 'i-[heroicons-outline/annotation]',
+      },
+      tagIcons: {
+        github: 'i-[carbon/logo-github]',
+        vue: 'i-[carbon/logo-vue]',
+      },
+    },
+    search: {
+      provider: 'local',
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Learn', link: '/learn/what-is-noir' },
       { text: 'Build', link: '/build/introduction/create-account' },
+      {
+        text: 'Blog',
+        items: [
+          {
+            text: 'Blog Home',
+            link: '/blog/',
+            activeMatch: '/blog/$',
+          },
+          {
+            text: 'Tags',
+            link: '/blog/tags',
+            activeMatch: '/blog/tags',
+          },
+          {
+            text: 'Archives',
+            link: '/blog/archives',
+            activeMatch: '/blog/archives',
+          }
+        ],
+      },
     ],
+
     sidebar: {
       "/learn/": [
         {
@@ -102,7 +140,10 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+      {
+        icon: 'github',
+        link: 'https://github.com/jcamp-code/vitepress-blog-theme',
+      },
+    ],
+  },
 })
